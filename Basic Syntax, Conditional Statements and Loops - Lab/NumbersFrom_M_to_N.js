c6ee4eef1f2961704c5d5a843628dde03933936a
Ridvan <ridvans@abv.bg>
2022-09-16 16:47:51 +0300

    Numbers ,sum of Number ...

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/NumbersFrom_M_to_N.js
@@ -0,0 +1,6 @@
+function numbers(firstNumber, secondNumber) {
+    for (let i = firstNumber; i >= secondNumber; i--) {
+        console.log(i);
+    }
+}
+numbers(6, 2)
---
+++ Basic Syntax, Conditional Statements and Loops - Lab/numberFrom_N_to_1.js
@@ -0,0 +1,6 @@
+function numbersFromN(number) {
+    for (let i = number; i > 0; i--) {
+        console.log(i)
+    }
+}
+numbersFromN(5)

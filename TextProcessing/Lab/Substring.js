3cbe8cb0b26e74f17ad838bb6a2ad21e8124443a
ridvanms <ridvans@abv.bg>
2022-11-20 13:48:19 +0200

    substring

---
+++ TextProcessing/Lab/Substring.js
@@ -0,0 +1,4 @@
+function letSubstring(word, startingIndex, countOfElements) {
+  console.log(word.substr(startingIndex, countOfElements));
+}
+letSubstring("ASentence", 1, 8);
---
+++ TextProcessing/censoredWords.js
@@ -0,0 +1,2 @@
+function censoredWords() {}
+censoredWords();

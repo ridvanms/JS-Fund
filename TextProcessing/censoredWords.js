385f8032184dd0d913fb4d547980611002ceb52b
ridvanms <ridvans@abv.bg>
2022-11-20 14:01:15 +0200

    censoredWord

--- TextProcessing/censoredWords.js
+++ TextProcessing/censoredWords.js
@@ -1,2 +1,9 @@
-function censoredWords() {}
-censoredWords();
+function censoredWords(text, word) {
+  let addingStars = "*".repeat(word.length);
+  let censored = text.replace(word, addingStars);
+  while (censored.includes(word)) {
+    censored = censored.replace(word, addingStars);
+  }
+  console.log(censored);
+}
+censoredWords("A small sentence with some words", "small");
